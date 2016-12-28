15e708f4841856859e54ffd73752d1511631b5b8
panfeng3141 <1131308065@qq.com>
2016-12-28 23:50:49 +0800

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -1,3 +1,5 @@
+#pydoc 帮助查看命令，例如,在命令行输入pydoc raw_input
+#raw_input(提示内容)
 age = raw_input("How old are you? ")
 height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
